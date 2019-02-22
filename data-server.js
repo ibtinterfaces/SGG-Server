3ae2e24fcdaca5504be934e531be551ef689dcb3
ibtinterfaces <nils.koehler@ibt-interfaces.de>
2019-02-22 13:54:08 +0100

    StarterList distributed on connection starterList is now stored in located in backend server ram

--- data-server.js
+++ data-server.js
@@ -1,15 +1,22 @@
 // Module: data-server.js   
 // v.1.0
 //
+var fs = require('fs');
+
+// import fileStartliste from './startliste.json'
 
 // Evermore
 const port = 8000
 
 var json // Buffer
 // empty config file
-var trackerConfig = [{
-    orgaselect: 0,
-}]
+// var trackerConfig = [{
+//     orgaselect: 0,
+// }]
+
+var orgaselect = 0
+var votedteam = 0
+var starterList = []
 
 const io = require("socket.io")
 const server = io.listen(port)
@@ -17,6 +24,9 @@ const server = io.listen(port)
 console.log('Websocket Server is listening on Port: '+port)
 // event fired every time a new client connects:
 server.on("connection", (socket) => {
+    server.emit('sync_orgaselect', orgaselect)
+    server.emit('sync_votedteam', votedteam)
+    server.emit('sync_starterlist', starterList)
     console.info(`Client connected [id=${socket.id}]`);
    
   
@@ -25,8 +35,35 @@ server.on("connection", (socket) => {
     });
   
     socket.on('sync_orgaselect', (data) => {
-        server.emit('sync_orgaselect', data)
-        console.info('Receive Data from vote Server');
+        orgaselect = data
+        server.emit('sync_orgaselect', orgaselect)
+        console.info(data);
+    });
+    socket.on('sync_votedteam', (data) => {
+        votedteam = data
+        server.emit('sync_votedteam', votedteam)
+        console.info(data);
+    });
+    socket.on('sync_final_results', (data) => {
+        server.emit('sync_final_results', data)
+        console.info(data);
+    });
+    
+    socket.on('sync_starterlist', (data) => {
+        starterList = data
+        server.emit('sync_starterlist', starterList)
+        // do local Backup FS or database
+
+        console.info(data);
+    });
+    socket.on('mobile_vote', (data) => {
+        server.emit('mobile_vote', data)
+        console.info('Receive vote data');
+        console.info(data);
+    });
+    socket.on('mobile_busy', (data) => {
+        server.emit('mobile_busy', data)
+        console.info('Receive busy data');
         console.info(data);
     });
     
